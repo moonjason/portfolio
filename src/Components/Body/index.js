0876611696543020ab5e72d4471228c0d3333cfc
Jason Moon <jasonmoonjm@gmail.com>
2019-11-11 12:37:10 -0800

    intial template

---
+++ src/Components/Body/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const Body = () => {
+    return (
+        <>
+            <h1>Body Component</h1>
+            <h3>About</h3>
+            <p>About Message</p>
+            <h3>Projects</h3>
+            <h4>Project 1</h4>
+            <p>Project Description</p>
+            <p>Technologies</p>
+        </>
+    )
+}
+
+export default Body;
---
+++ src/Components/Header/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <>
+            <h1>Header Component</h1>
+            <p>Picture</p>
+            <p>Welcome Message</p>
+            <p>Links</p>
+        </>
+    )
+}
+
+export default Header;
