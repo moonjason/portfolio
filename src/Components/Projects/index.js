957516fc00d09db170bc1d306970f65434bfc646
moonjason <jasonmoonjm@gmail.com>
2019-12-14 17:56:34 -0800

    projects component mobile responsiveness

--- src/Components/Projects/index.js
+++ src/Components/Projects/index.js
@@ -1,5 +1,9 @@
 import React from 'react';
-import { ProjectItem } from './style';
+import { 
+    ProjectItem,
+    ProjectImg,
+    ProjectDesc
+ } from './style';
 
 import AOS from 'aos';
 import 'aos/dist/aos.css'; // You can also use <link> for styles
@@ -7,16 +11,38 @@ import 'aos/dist/aos.css'; // You can also use <link> for styles
 AOS.init();
 
 const style = {
-    "width": "90%",
+    "width": "85%",
+    'height': '100%',
     "maxWidth": "120rem",
-    "margin": "3rem auto",
+    "margin": "0 auto",
+    'position': 'relative'
 }
 
+const hasMarginRight = {'marginRight': '6rem'}
+
 const Projects = () => {
-    return (    
+    return (
         <div style={style}>
-            <h1>Projects</h1>
-            <ProjectItem></ProjectItem>
+            <h1 className="Heading">Projects</h1> 
+            <ProjectItem>
+                <ProjectImg style={hasMarginRight}>
+                    <img style={{'width': '100%', 'imageOrientation': 'from-image'}} src="./images/gotnext-ss.png" alt=""/>
+                </ProjectImg>
+                <ProjectDesc>
+                    <h1>GotNext</h1>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Nisl pretium fusce id velit ut tortor. Quam id leo in vitae turpis massa sed elementum. Consequat ac felis donec et odio pellentesque diam volutpat. Consectetur adipiscing elit ut aliquam purus sit. Adipiscing vitae proin sagittis nisl rhoncus mattis rhoncus. Eget duis at tellus at. Faucibus purus in massa tempor nec feugiat. Purus in massa tempor nec feugiat nisl pretium. Commodo quis imperdiet massa tincidunt. Scelerisque in dictum non consectetur a erat nam at. Iaculis at erat pellentesque adipiscing commodo elit at.</p>
+                    <p>Technologies Used </p>
+                </ProjectDesc>
+            </ProjectItem>
+            <ProjectItem>
+                <ProjectDesc style={hasMarginRight}>
+                    <h1>GotNext</h1>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Nisl pretium fusce id velit ut tortor. Quam id leo in vitae turpis massa sed elementum. Consequat ac felis donec et odio pellentesque diam volutpat. Consectetur adipiscing elit ut aliquam purus sit. Adipiscing vitae proin sagittis nisl rhoncus mattis rhoncus. Eget duis at tellus at. Faucibus purus in massa tempor nec feugiat. Purus in massa tempor nec feugiat nisl pretium. Commodo quis imperdiet massa tincidunt. Scelerisque in dictum non consectetur a erat nam at. Iaculis at erat pellentesque adipiscing commodo elit at.</p>
+                    <p>Technologies Used </p>
+                </ProjectDesc>
+                <ProjectImg>
+                </ProjectImg>
+            </ProjectItem>
         </div>
     )
 }
--- src/Components/Projects/style.js
+++ src/Components/Projects/style.js
@@ -4,4 +4,27 @@ export const ProjectItem = styled.div`
     display: flex;
     align-items: center;
     justify-content: center;
+    margin-bottom: 5rem;
+    @media (max-width: 850px) {
+        flex-direction: column;
+    }
+`
+
+export const ProjectImg = styled.div`
+    max-width: 50rem;
+    flex: 1;
+    border: 1px solid black;
+    @media (max-width: 850px){
+        width: 25rem;
+        margin: 0 0 0 6rem;
+        order: 1;
+    }
+`
+
+export const ProjectDesc = styled.div`
+    flex: 1;
+    @media (max-width: 850px) {
+        margin: 0 !important;
+        order: 2;
+    }
 `
