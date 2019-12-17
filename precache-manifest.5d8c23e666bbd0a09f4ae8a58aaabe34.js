825d80b21af03a61cfab94e70204601d7d341a00
moonjason <jasonmoonjm@gmail.com>
2019-12-16 22:24:07 -0800

    Updates

---
+++ precache-manifest.5d8c23e666bbd0a09f4ae8a58aaabe34.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fbc8227769d88f17622e6893727442a9",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "06d0172b4aaa5a805498",
+    "url": "/portfolio/static/css/2.7abef457.chunk.css"
+  },
+  {
+    "revision": "d9cd1794275f05b3256f",
+    "url": "/portfolio/static/css/main.beeeffca.chunk.css"
+  },
+  {
+    "revision": "06d0172b4aaa5a805498",
+    "url": "/portfolio/static/js/2.26942c0e.chunk.js"
+  },
+  {
+    "revision": "d9cd1794275f05b3256f",
+    "url": "/portfolio/static/js/main.8f1877d9.chunk.js"
+  },
+  {
+    "revision": "6b26cb3bc1b9147ba086",
+    "url": "/portfolio/static/js/runtime-main.86a05020.js"
+  }
+]);
