b102013a2b6197148a7cb7e39744386f88a0b05c
moonjason <jasonmoonjm@gmail.com>
2019-12-21 17:40:04 -0800

    Updates

--- precache-manifest.ea97552095e3a197ec994c3af258a500.js
+++ precache-manifest.ea97552095e3a197ec994c3af258a500.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "fbc8227769d88f17622e6893727442a9",
+    "revision": "c366e0899362e6c395a9efdaa846ce96",
     "url": "/portfolio/index.html"
   },
   {
@@ -8,16 +8,16 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/portfolio/static/css/2.7abef457.chunk.css"
   },
   {
-    "revision": "d9cd1794275f05b3256f",
-    "url": "/portfolio/static/css/main.beeeffca.chunk.css"
+    "revision": "ea2512f0535f484ad115",
+    "url": "/portfolio/static/css/main.63c2dc92.chunk.css"
   },
   {
     "revision": "06d0172b4aaa5a805498",
     "url": "/portfolio/static/js/2.26942c0e.chunk.js"
   },
   {
-    "revision": "d9cd1794275f05b3256f",
-    "url": "/portfolio/static/js/main.8f1877d9.chunk.js"
+    "revision": "ea2512f0535f484ad115",
+    "url": "/portfolio/static/js/main.d90bf70b.chunk.js"
   },
   {
     "revision": "6b26cb3bc1b9147ba086",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.5d8c23e666bbd0a09f4ae8a58aaabe34.js"
+  "/portfolio/precache-manifest.ea97552095e3a197ec994c3af258a500.js"
 );
 
 self.addEventListener('message', (event) => {
